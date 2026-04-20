0d092c0ec780fc18335d3f1c560b60098e2508f9
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-04-21 00:22:51 +0330

    feat(layout): add layout for small screen

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Layout from "@/components/layout/Layout";
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const vazirmatn = Vazirmatn({
+  variable: "--font-vazirmatn",
+  // subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -18,8 +16,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="fa" dir="rtl" className={`${vazirmatn.variable}`}>
+      <body>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,65 @@
-import loginBtn from "@/assets/icons/src/sign in buttom.svg"
-const Layout = ({children}) => {
+import loginBtn from "@/assets/icons/sign-in-buttom.svg";
+import airaLogo from "@/assets/images/aira-logo.svg";
+import caoLogo from "@/assets/images/cao-logo.svg";
+import caoPaxrightsLogo from "@/assets/images/cao-paxrights-logo.svg";
+import ecunionLogo from "@/assets/images/ecunion-logo.svg";
+import samandehiLogo from "@/assets/images/samandehi-logo.svg";
+import torinoLogo from "@/assets/images/Torino.svg";
+import Image from "next/image";
+import styles from "./Layout.module.css";
+const Layout = ({ children }) => {
   return (
     <>
-    <header>
-      <img src={loginBtn} alt="login-btn" />
-    </header>
+      <header className={styles.header}>
+        <div className={styles.menuBtn}></div>
+        <Image width={40} height={40} src={loginBtn} alt="login-btn" />
+      </header>
+      <div className={styles.content}>{children}</div>
+      <footer className={styles.footer}>
+        {/* <div className={styles['footer-border']}></div> */}
+        <div className={styles.top}>
+          <div className={styles.top_start}>
+            <h2>تورینو</h2>
+            <ul>
+              <li>درباره ما</li>
+              <li>تماس با ما</li>
+              <li>چرا تورینو</li>
+              <li>بیمه مسافرتی</li>
+            </ul>
+          </div>
+
+          <div className={styles.top_end}>
+            <h2>خدمات مشتریان</h2>
+            <ul>
+              <li>پشتیبانی آنلاین</li>
+              <li>راهنمای خرید</li>
+              <li>راهنمای استرداد</li>
+              <li>پرسش و پاسخ</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.bottom_start}>
+            <Image width={35} height={38} src={ecunionLogo} alt="login-btn" />
+            <Image width={35} height={38} src={samandehiLogo} alt="login-btn" />
+            <Image width={35} height={38} src={airaLogo} alt="login-btn" />
+            <Image width={35} height={38} src={caoLogo} alt="login-btn" />
+            <Image
+              width={35}
+              height={38}
+              src={caoPaxrightsLogo}
+              alt="login-btn"
+            />
+          </div>
+          <div className={styles.bottom_end}>
+            <Image width={100} height={30} src={torinoLogo} alt="login-btn" />
+            <p className={styles.support_number}>تلفن پشتیبانی: 021-8574</p>
+          </div>
+        </div>
+        <p className={styles.copyright}>کلیه حقوق این وب سایت متعلق به تورینو میباشد.</p>
+      </footer>
     </>
+  );
+};
 
-  )
-}
-
-export default Layout
+export default Layout;
