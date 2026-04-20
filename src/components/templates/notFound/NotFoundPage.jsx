d1ea4dc8d89b5193c9c99d099358ab5bb29a24d9
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-04-21 01:16:37 +0330

    feat(error-404): add not-found page

---
+++ src/components/templates/notFound/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import error404 from "@/assets/images/error-404.svg"
+import Image from "next/image"
+import styles from "./NotFoundPage.module.css"
+import Link from "next/link"
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.container}>
+      <Image width={322} height={322} src={error404} alt="error 404 image" />
+      <h2 className={styles.title}>صفحه مورد نظر یافت نشد!</h2>
+      <Link href="/" className={styles.backToHomeBtn}>بازگشت به صفحه اصلی</Link>
+    </div>
+  )
+}
+
+export default NotFoundPage
