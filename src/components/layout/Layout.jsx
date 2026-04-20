aa85e8bf2a72a68c8da01f0ffec2edcd047743b6
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-04-20 21:14:51 +0330

    Initial commit from Create Next App

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import loginBtn from "@/assets/icons/src/sign in buttom.svg"
+const Layout = ({children}) => {
+  return (
+    <>
+    <header>
+      <img src={loginBtn} alt="login-btn" />
+    </header>
+    </>
+
+  )
+}
+
+export default Layout
