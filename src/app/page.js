21704e2f847dd2c10ccc1d45e7eb63ef7ed96957
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-04-23 01:14:14 +0330

    feat(search): add search-box component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Banner from "@/components/modules/banner/Banner";
 import Features from "@/components/modules/features/Features";
+import SearchBox from "@/components/modules/searchBox/SearchBox";
 import Telesales from "@/components/modules/telesales/Telesales";
 import ToursList from "@/components/modules/toursList/ToursList";
 import WhyUs from "@/components/modules/whyUs/WhyUs";
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <Banner />
       <div style={{ padding: "0 31px" }}>
+        <SearchBox />
         <ToursList />
         <Telesales />
         <WhyUs />
---
+++ src/components/modules/searchBox/SearchBox.jsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+import styles from "./SearchBox.module.css";
+
+const SearchBox = () => {
+  const [isFocus, setIsFocus] = useState(false);
+
+  const focusHandler = (e) => {
+    if (!e.target.value) setIsFocus(true);
+  };
+
+  const blurHandler = (e) => {
+    if (!e.target.value) setIsFocus(false);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputWrapper}>
+        <input type="text" onFocus={focusHandler} onBlur={blurHandler} />
+        {!isFocus && (
+          <div className={styles.label}>
+            <i class="fa-solid fa-location-dot"></i>
+            مبدا
+          </div>
+        )}
+      </div>
+      <div className={styles.inputWrapper}>
+        <input type="text" onFocus={focusHandler} onBlur={blurHandler} />
+        {!isFocus && (
+          <div className={styles.label}>
+            <i class="fa-solid fa-globe"></i>
+            مقصد
+          </div>
+        )}
+      </div>
+      <div className={styles.inputWrapper}>
+        <input type="text" onFocus={focusHandler} onBlur={blurHandler} />
+        {!isFocus && (
+          <div className={styles.label}>
+            <i class="fa-solid fa-calendar-days"></i>
+            تاریخ
+          </div>
+        )}
+      </div>
+
+      <button>جستجو</button>
+    </div>
+  );
+};
+
+export default SearchBox;
