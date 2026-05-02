4d393d62f48715c23641a9acbd0dee07d44bb5be
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-05-02 10:17:08 +0330

    feat(constant): add vehicles data

---
+++ src/constants/vehicles.js
@@ -0,0 +1,22 @@
+export const VEHICLES = {
+  Bus: {
+    name: "اتوبوس",
+    icon: <i className="fas fa-bus-alt"></i>,
+  },
+  Train: {
+    name: "قطار",
+    icon: <i className="fas fa-train-subway"></i>,
+  },
+  Airplane: {
+    name: "هواپیما",
+    icon: <i className="fas fa-plane"></i>,
+  },
+  SUV: {
+    name: "شاسی‌بلند",
+    icon: <i className="fas fa-car"></i>,
+  },
+  Van: {
+    name: "ون",
+    icon: <i className="fas fa-shuttle-van"></i>,
+  },
+};
