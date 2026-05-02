f5ebdc9b780307b7d7786a794b735cd48c38521b
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-05-02 10:41:34 +0330

    feat(constant): add places data

---
+++ src/constants/places.js
@@ -0,0 +1,13 @@
+const PLACES = {
+  Sanandaj: "سنندج",
+  Madrid: "مادرید",
+  Tehran: "تهران",
+  Isfahan: "اصفهان",
+  sulaymaniyahTour: "سلیمانیه",
+  Hewler: "اربیل",
+  Mazandaran: "مازندران",
+  "offRoad Center": "مرکز آفرود",
+  Italy: "ایتالیا",
+};
+
+export { PLACES };
