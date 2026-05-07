533472d511aec91b828419df333aadfd97bb2bbe
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-05-08 00:33:26 +0330

    refactor(WhyUs): rename component

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Features from "@/components/modules/Features";
 import SearchBox from "@/components/modules/SearchBox";
 import Telesales from "@/components/modules/Telesales";
 import ToursList from "@/components/modules/ToursList";
-import WhyUs from "@/components/modules/whyUs";
+import WhyUs from "@/components/modules/WhyUs";
 import { serverFetch } from "@/core/services/http";
 
 export default async function Home({ searchParams }) {
