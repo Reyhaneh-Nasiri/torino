e7af729e07ae84a1522a9beaa38cacb63e6941d5
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-05-08 18:30:46 +0330

    feat(tour-card): display numbers in Persian and price formatting

--- src/components/modules/TourCard/index.js
+++ src/components/modules/TourCard/index.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "@/core/utils/currency";
+import { e2p } from "@/core/utils/digit";
 import Link from "next/link";
 import styles from "./index.module.css";
 
@@ -7,14 +9,14 @@ const TourCard = ({ id, title, image, price }) => {
       <img width={327.44} height={159} src={image} alt={title} />
       <h3 className={styles.name}>{title}</h3>
       <p className={styles.description}>
-        مهر ماه . 3 روزه - پرواز - هتل 3 س...
+        {e2p(" مهر ماه . 3 روزه - پرواز - هتل 3 س...")}
       </p>
       <div className={styles.cardFooter}>
         <Link href={`/tours/${id}`} className={styles.cardBtn}>
           رزرو
         </Link>
         <p>
-          <span className={styles.price}>{price}</span>{" "}
+          <span className={styles.price}>{formatCurrency(price)}</span>{" "}
           <span className={styles.unit}>تومان</span>
         </p>
       </div>
