90077558f4ea212ec9d190f1d4b88a0afbc2f872
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2026-05-31 17:00:16 +0330

    refactor(404-page): refactor 404 page styles using responsive units and CSS variables
    - Migrate hardcoded values to design system variables
    - Implement `clamp()` for fluid typography, dynamic padding, and adaptive margins.
    - Add responsive layout adjustments for desktop view using flexbox.
    - Integrate a dedicated image wrapper with `aspect-ratio` for responsive image scaling.

--- src/components/templates/NotFound/index.js
+++ src/components/templates/NotFound/index.js
@@ -1,16 +1,22 @@
-import error404 from "@/assets/images/error-404.svg"
-import Image from "next/image"
-import styles from "./index.module.css"
-import Link from "next/link"
+import error404 from "@/assets/images/error-404.svg";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./index.module.css";
 
 const NotFoundPage = () => {
   return (
     <div className={`${styles.container} content-boxed`}>
-      <Image width={322} height={322} src={error404} alt="error 404 image" />
-      <h2 className={styles.title}>صفحه مورد نظر یافت نشد!</h2>
-      <Link href="/" className={styles.backToHomeBtn}>بازگشت به صفحه اصلی</Link>
+      <div className={styles.imageWrapper}>
+        <Image fill priority src={error404} alt="error 404 image" />
+      </div>
+      <div className={styles.content}>
+        <h2 className={styles.title}>صفحه مورد نظر یافت نشد!</h2>
+        <Link href="/" className={styles.backToHomeBtn}>
+          بازگشت به صفحه اصلی
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
